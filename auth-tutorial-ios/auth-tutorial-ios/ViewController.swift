7e214ea3502741443c69f4c08cef83f6a4873c1d
bughana <hannah.teuteberg@gmx.de>
2018-10-03 17:49:12 +0200

    ADD: initial files

---
+++ auth-tutorial-ios/auth-tutorial-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  auth-tutorial-ios
+//
+//  Created by Test User on 03.10.18.
+//  Copyright © 2018 12plus1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
