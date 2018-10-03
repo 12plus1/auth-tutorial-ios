f6da690055a87af63b51d97f2fc1e04cffd0e3ad
bughana <hannah.teuteberg@gmx.de>
2018-10-03 18:28:54 +0200

    ADD: authservice

---
+++ auth-tutorial-ios/auth-tutorial-ios/AuthService.swift
@@ -0,0 +1,41 @@
+import Foundation
+import AppAuth
+
+protocol AuthServiceError: Error {}
+
+class AuthService {
+    struct FailedToAuthenticate: AuthServiceError, UnderlyingError { let underlying: Error? }
+    
+    var currentAuthFlow: OIDExternalUserAgentSession?
+    private var authState: OIDAuthState?
+    
+    private let authEndpoint = URL(string: "http://localhost:4444/oauth2/auth")!
+    private let tokenEndpoint = URL(string: "http://localhost:4444/oauth2/token")!
+    private let redirectURI = URL(string: "com.example.app:/oauth2/callback")!
+    private let clientId = "test-client"
+    private let clientSecret = "test-secret"
+    private lazy var config = OIDServiceConfiguration(authorizationEndpoint: authEndpoint, tokenEndpoint: tokenEndpoint)
+    
+    func authenticateUser(from vc: UIViewController, onSuccess: @escaping (OIDAuthState) -> Void, onError: @escaping (Error) -> Void) {
+        let request = OIDAuthorizationRequest(configuration: config,
+                                              clientId: clientId,
+                                              clientSecret: clientSecret,
+                                              scopes: [OIDScopeOpenID, "offline"],
+                                              redirectURL: redirectURI,
+                                              responseType: OIDResponseTypeCode,
+                                              additionalParameters: nil)
+        currentAuthFlow = OIDAuthState.authState(byPresenting: request, presenting: vc, callback: { (authState, error) in
+            if let authState = authState {
+                onSuccess(authState)
+            } else {
+                onError(FailedToAuthenticate(underlying: error))
+            }
+        })
+    }
+}
+
+protocol UnderlyingError: Error {
+    var underlying: Error? { get }
+}
+
+protocol LocalizedUnderlyingError: LocalizedError, UnderlyingError {}
