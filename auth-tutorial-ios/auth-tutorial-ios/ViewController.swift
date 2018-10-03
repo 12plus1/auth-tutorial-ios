7a96bed0c4ed02aa477e87870ae10cc4495267c4
bughana <hannah.teuteberg@gmx.de>
2018-10-03 17:59:04 +0200

    ADD: necessary views

--- auth-tutorial-ios/auth-tutorial-ios/ViewController.swift
+++ auth-tutorial-ios/auth-tutorial-ios/ViewController.swift
@@ -1,25 +1,12 @@
-//
-//  ViewController.swift
-//  auth-tutorial-ios
-//
-//  Created by Test User on 03.10.18.
-//  Copyright © 2018 12plus1. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var successLabel: UILabel!
+    
+    @IBAction func authenticate(_ sender: Any) {
+        loginButton.isHidden = true
+        successLabel.isHidden = false
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
