710810eb7a47cd65e69b4402c812c795fe1421d0
Oliver Roesner <belancr@gmail.com>
2018-10-09 15:09:37 +0200

    MOD: Initiate register flow on button click

--- auth-tutorial-ios/auth-tutorial-ios/ViewController.swift
+++ auth-tutorial-ios/auth-tutorial-ios/ViewController.swift
@@ -3,10 +3,15 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var successLabel: UILabel!
+    let authService = AuthService()
     
     @IBAction func authenticate(_ sender: Any) {
-        loginButton.isHidden = true
-        successLabel.isHidden = false
+        authService.authenticateUser(from: self, onSuccess: { [weak self] authState in
+            self?.loginButton.isHidden = true
+            self?.successLabel.isHidden = false
+        }, onError: { error in
+            print(error)
+        })
     }
 }
 
