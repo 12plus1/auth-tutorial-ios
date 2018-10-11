3fbaec97df29d6e9ba9aa622fa71cd507a6c6175
bughana <hannah.teuteberg@gmx.de>
2018-10-11 15:06:01 +0200

    DEL: error mapping

--- auth-tutorial-ios/auth-tutorial-ios/AuthService.swift
+++ auth-tutorial-ios/auth-tutorial-ios/AuthService.swift
@@ -1,16 +1,12 @@
 import Foundation
 import AppAuth
 
-protocol AuthServiceError: Error {}
-
 class AuthService {
-    struct FailedToAuthenticate: AuthServiceError, UnderlyingError { let underlying: Error? }
-    
     var currentAuthFlow: OIDExternalUserAgentSession?
     
     private let authEndpoint = URL(string: "http://localhost:4444/oauth2/auth")!
     private let tokenEndpoint = URL(string: "http://localhost:4444/oauth2/token")!
-    private let redirectURI = URL(string: "com.example.app:/oauth2/callback")!
+    private let redirectURL = URL(string: "com.example.app:/oauth2/callback")!
     private let clientId = "test-client"
     private let clientSecret = "test-secret"
     private lazy var config = OIDServiceConfiguration(authorizationEndpoint: authEndpoint, tokenEndpoint: tokenEndpoint)
@@ -20,22 +16,17 @@ class AuthService {
                                               clientId: clientId,
                                               clientSecret: clientSecret,
                                               scopes: [OIDScopeOpenID, "offline"],
-                                              redirectURL: redirectURI,
+                                              redirectURL: redirectURL,
                                               responseType: OIDResponseTypeCode,
                                               additionalParameters: nil)
         currentAuthFlow = OIDAuthState.authState(byPresenting: request, presenting: vc, callback: { (authState, error) in
             if let authState = authState {
                 print(authState)
                 onSuccess(authState)
-            } else {
-                onError(FailedToAuthenticate(underlying: error))
+            } else if let error = error {
+                print(error)
+                onError(error)
             }
         })
     }
 }
-
-protocol UnderlyingError: Error {
-    var underlying: Error? { get }
-}
-
-protocol LocalizedUnderlyingError: LocalizedError, UnderlyingError {}
